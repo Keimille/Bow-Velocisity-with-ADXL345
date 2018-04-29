186d9ee8de85b7608c14b46ae481161bdd2017db
Keith Miller <36780654+Keimille@users.noreply.github.com>
2018-04-29 16:25:09 -0400

    Update Velocity.c

--- Velocity.c
+++ Velocity.c
@@ -14,6 +14,8 @@ xi = 0;
 printf("Enter the amount of seconds it took for you to get to final position");
 scanf("%d", &t);
 Velocity = xf - xi / t;
+  /* Will likely need to create VelocityX, VelocityY and VelocityZ to record velocity of all axis
+  the formula remains the same as line 16 */
 printf("Velocity is %d - %d / %d = %d meters per second", xf,xi,t, Velocity);
 
 return 0;    
